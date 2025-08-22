ea57a61f1df4bd8963de93c89dfcb605fc6d411d
PriyanshVaishnav21 <priyanshvaishnav18@gmail.com>
2025-08-22 17:49:35 +0530

    initial commit

--- backend/populateDB.js
+++ backend/populateDB.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const Question = require("./Question"); // Adjust the path as necessary to your Question model
 const { data } = require("./data"); // Adjust the path to where your data.js file is located
 
+
+
 const mongoURI = process.env.MONGO_URI;
 // const mongoURI = 'mongodb+srv://${username}:${password}@cluster0.3x0s7hh.mongodb.net/?appName=mongosh+2.1.5';
 
